d6a2f9e67a3d747c8de61f4365551511504e3740
Amanuel Zewdu <amanuelzewdu21@gmail.com>
2022-08-09 15:17:11 +0300

    4th commit cleen tweets modified

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -9,29 +9,32 @@ class Clean_Tweets:
         self.df = df
         print('Automation in Action...!!!')
         
-    def drop_unwanted_column(self, df:pd.DataFrame)->pd.DataFrame:
-        """
-        remove rows that has column names. This error originated from
-        the data collection stage.  
-        """
-        unwanted_rows = df[df['retweet_count'] == 'retweet_count' ].index
-        df.drop(unwanted_rows , inplace=True)
-        df = df[df['polarity'] != 'polarity']
-        
-        return df
+    def drop_unwanted_column(self) -> pd.DataFrame:
+        columns = ['created_at', 'source', 'original_text', 'retweet_text', 'sentiment', 'polarity',
+                   'subjectivity', 'lang', 'statuses_count', 'favorite_count', 'retweet_count',
+                   'screen_name', 'followers_count', 'friends_count', 'possibly_sensitive',
+                   'hashtags', 'user_mentions', 'location']
+        unwanted_rows = []
+        for columnName in columns:
+            unwanted_rows = self.df[self.df[columnName] == columnName].index
+            self.df.drop(unwanted_rows, inplace=True)
+            self.df.reset_index(drop=True, inplace=True)
+        return self.df
+
     def drop_duplicate(self, df:pd.DataFrame)->pd.DataFrame:
         """
         drop duplicate rows
         """
         
         df.drop_duplicates(inplace=True)
+        df.reset_index(drop=True, inplace=True)
         
         return df
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert column to datetime
         """
-        df['created_at'] = pd.to_datetime(df['created_at'])
+        df['created_at'] = pd.to_datetime(df['created_at'], errors='coerce')
         
         return df
     
@@ -40,10 +43,20 @@ class Clean_Tweets:
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
+
+        """
+        Here sentiment, Statuses_count, followers_count, 
+        and friends_count are added.
+        """
+
         df['polarity'] = pd.to_numeric(df['polarity'])
         df['Subjectivity'] = pd.to_numeric(df['Subjectivity'])
         df['retweet_count'] = pd.to_numeric(df['retweet_count'])
         df['favourite_count'] = pd.to_numeric(df['favourite_count'])
+        df['sentiment'] = pd.to_numeric(df['sentiment'])
+        df['statuses_count'] = pd.to_numeric(df['statuses_count'])
+        df['followers_count'] = pd.to_numeric(df['followers_count'])
+        df['friends_count'] = pd.to_numeric(df['friends_count'])
 
         return df
     
@@ -52,6 +65,7 @@ class Clean_Tweets:
         remove non english tweets from lang
         """
         
-        df = df.drop(df[df['lang'] != 'en'].index)
-        
-        return df
+        index_names = self.df[self.df['lang'] != 'en'].index
+        self.df.drop(index_names, inplace=True)
+        self.df.reset_index(drop=True, inplace=True)
+        return self.df
