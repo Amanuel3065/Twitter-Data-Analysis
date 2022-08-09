919eba0871144318164c790e0d8927b56dc83102
Amanuel Zewdu <amanuelzewdu21@gmail.com>
2022-08-09 12:13:02 +0300

    modify extract_dataframe.py

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -1,7 +1,7 @@
 import json
 import pandas as pd
 from textblob import TextBlob
-
+import re
 
 def read_json(json_file: str)->list:
     """
@@ -36,14 +36,28 @@ class TweetDfExtractor:
 
     # an example function
     def find_statuses_count(self)->list:
-        statuses_count 
+        statuses_count = [tw['user']['statuses_count']
+                            for  tw in self.tweets_list]
         
     def find_full_text(self)->list:
-        text = 
-       
+        text = []
+        for tw in self.tweet_list:
+            try:
+                text.append(tw['retweeted_status']
+                            ['extended_tweet']['full_text'])
+            except KeyError:
+                text.append(tw['text'])
+        return text
     
     def find_sentiments(self, text)->list:
-        
+        polarity = []
+        subjectivity = []
+
+        for i in text:
+            if(i):
+                polarity.append(TextBlob(str(i)).polarity)
+                subjectivity.append(TextBlob(str(i)).subjectivity)
+
         return polarity, self.subjectivity
 
     def find_created_time(self)->list:
@@ -51,39 +65,78 @@ class TweetDfExtractor:
         return created_at
 
     def find_source(self)->list:
-        source = 
+        source = [tw.get('source',None)
+                    for tw in self.tweets_list]
 
         return source
 
     def find_screen_name(self)->list:
-        screen_name = 
+        screen_name = [tw.get('user', {}).get('screen_name',None)
+                        for tw in self.tweets_list]
+        return screen_name
 
     def find_followers_count(self)->list:
-        followers_count = 
-
+        followers_count = [tw.get('user', {}).get('followers_count',None)
+                        for tw in self.tweets_list]
+        return followers_count
     def find_friends_count(self)->list:
-        friends_count = 
+        friends_count = [tw.get('user', {}).get('friends_count',None)
+                        for tw in self.tweets_list]
+        return friends_count
 
     def is_sensitive(self)->list:
         try:
-            is_sensitive = [x['possibly_sensitive'] for x in self.tweets_list]
+            is_sensitive = [tw['retweeted_status']['possibly_sensitive'] for tw in self.tweets_list]
         except KeyError:
-            is_sensitive = None
+            is_sensitive = [None
+                            for tw in self.tweets_list]
 
         return is_sensitive
 
     def find_favourite_count(self)->list:
+        favourite_count = [tw.get('retweeted_status', {}).get('favourite_count',None)
+                        for tw in self.tweets_list]
         
     
     def find_retweet_count(self)->list:
-        retweet_count = 
+        retweet_count = [tw.get('retweeted_status', {}).get('retweet_count',None)
+                        for tw in self.tweets_list]
 
     def find_hashtags(self)->list:
-        hashtags =
+        hashtags = [tw.get('entities', {}).get('hashtags',None)
+                        for tw in self.tweets_list]
+        _tags = []
+        hashtag_list_obj in all_hashtag_objs:
+        if(hash_tag_obj):
+            cur_hashtags = []
+            for hashtag_obj in hashtag_list_obj:
+                try:
+                    cur_hashtags.append(hashtag_obj['text'])
+                except KeyError:
+                    pass
+            hash_tags.append("".join(cur_hashtags))
+        else:
+            hash_tags.append(None)
+
+        return hash_tags
+
 
     def find_mentions(self)->list:
-        mentions = 
+        all_mentions_objs = [tw.get('entities', {}).get('user_mentions', None)
+                    for tw in self.tweets_list]
+        mentions = []
+        for mention_list_obj in all_mentions_objs:
+            if(mention_list_obj):
+                cur_mentions = []
 
+                for mention_obj in mention_list_obj:
+                    try:
+                        cur_mentions.append(mention_obj['screen_name',None])
+                    except KeyError:
+                        pass
+                mentions.append(None)
+
+        return mentions
 
     def find_location(self)->list:
         try:
